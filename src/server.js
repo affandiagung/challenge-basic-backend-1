84b69e879c54eff93bbeb59fa071f84d860f5006
affandi-cetta <affandi@cetta.tech>
2026-01-01 01:37:00 +0700

    Remove warning and add Mailpit App

--- src/server.js
+++ src/server.js
@@ -5,9 +5,9 @@ const app = require("./app");
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Swagger running on http://localhost:${PORT}/api/docs`);
-    console.log(`BACKEND API's running on http://localhost:${PORT}/api`);
-    console.log(`FRONTEND app running on http://localhost:${PORT}`);
+    console.log(`Swagger Docs running on : http://localhost:${PORT}/api/docs`);
+    console.log(`FRONTEND app running on : http://localhost:${PORT}`);
+    console.log(`Mailpit app running on : http://localhost:8025`);
     console.log(`
     ██╗███╗   ██╗ ██████╗ ███████╗████████╗██╗███╗   ██╗    ██████╗ ██╗    ██╗
     ██║████╗  ██║██╔════╝ ██╔════╝╚══██╔══╝██║████╗  ██║    ██╔════╝ ██║    ██║
